fdbd753671a8967ad3438e77238c6b43831e7a84
Kovacs <benedek.kovacs.ext@evosoft.com>
2026-01-22 12:28:23 +0100

    Set up constructor and get in dog service

--- src/app/services/dog.ts
+++ src/app/services/dog.ts
@@ -1,8 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Dog {
-  
+export class DogService {
+  private apiUrl = 'https://dog.ceo/api';
+
+  constructor(private http: HttpClient) {}
+
+  getDog(): Observable<any> {
+    return this.http.get('${this.apiUrl}/breeds/image/random');
+  }
 }
